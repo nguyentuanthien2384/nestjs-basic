dde7b1351c60a04660d72dd74cceddad6ebdeb6b
thien nt <thiennt2004@gmail.com>
2026-09-16 10:44:04 +0700

    feat: integrate mongoose and add user controller in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://thiennt2004_db_user:9Dj7ACvMxqAKeg8L@cluster0.lbolann.mongodb.net',
+    ),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
