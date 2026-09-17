7c669eb384c6413d50578767f179288bf4177666
thien nt <thiennt2004@gmail.com>
2026-09-17 13:22:10 +0700

    feat: implement users module with DTO validation and bcrypt password hashing

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,13 @@
-export class CreateUserDto {}
+// data transfer object
+import { IsEmail, IsNotEmpty } from 'class-validator';
+export class CreateUserDto {
+  @IsEmail({}, { message: 'Email không đúng định dạng' })
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty({ message: 'Password không được để trống' })
+  password: string;
+
+  name: string;
+  address: string;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync('B4c0//', salt);
+    return hash;
+  };
+  async create(nopiko: CreateUserDto) {
+    // async create(email: string, password: string, name: string) {
+    const hashPassword = this.getHashPassword(nopiko.password);
+    let user = await this.userModel.create({
+      email: nopiko.email,
+      password: hashPassword,
+      name: nopiko.name,
+    });
+    return user;
   }
 
   findAll() {
