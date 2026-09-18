948497b7bdbc9700fa01f9491e73aaea06284f2a
thien nt <thiennt2004@gmail.com>
2026-09-18 13:13:45 +0700

    feat: implement authentication module with passport jwt and local strategies

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 
 @Controller() //  route /
 export class AppController {
@@ -14,6 +14,7 @@ export class AppController {
 
   ) { }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
@@ -21,6 +22,7 @@ export class AppController {
   }
 
   // @UseGuards(JwtAuthGuard)
+  @Public()
   @Get('profile')
   getProfile(@Request() req) {
     return req.user;
